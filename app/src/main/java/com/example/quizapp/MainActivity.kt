79b5b16b7fedd0c3686ba10a6cc088ae005c64ee
Tony Gonzalez <tonygllambia@gmail.com>
2021-03-07 22:22:39 -0600

    home-screen

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
